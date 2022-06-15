cba8bf83880a636d23d51a413c1285216db356ee
jbl <jbl@debian-dev-jbl.debian-dev-jbl.pokus.io>
2022-06-15 09:47:30 -0400

    fixed docker-compose install from binaries, validated with v2.6. 0

--- debian11/provision-docker-compose.sh
+++ debian11/provision-docker-compose.sh
@@ -6,10 +6,10 @@ export D_COMPOSE_VERSION=${D_COMPOSE_VERSION:-"2.6.0"}
 
 # ---
 # Get the precompiled binary :
-wget  https://github.com/docker/compose/releases/download/${D_COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m)
+wget  https://github.com/docker/compose/releases/download/v${D_COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m)
 # ---
 # Install the binary into the GNU/Linux system :
-export D_COMPOSE_BIN_LOCALFILE=./bin/docker-compose-$(uname -s)-$(uname -m)
+export D_COMPOSE_BIN_LOCALFILE=./docker-compose-$(uname -s)-$(uname -m)
 sudo mv ${D_COMPOSE_BIN_LOCALFILE} /usr/bin/docker-compose
 sudo chmod +x /usr/bin/docker-compose
 export PATH=$PATH:/usr/bin/docker-compose
